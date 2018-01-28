1cb00d1eb2962659b0dcc79732c7cca51bed5a93
Kris Wilson <kristofor.wilson@gmail.com>
2018-01-28 09:41:17 -0800

    Alternate solution using for loop and not storing temporary variable.

--- Web-Dev-001v1/Unit 2/Lesson 6-5/FindByID.js
+++ Web-Dev-001v1/Unit 2/Lesson 6-5/FindByID.js
@@ -1,50 +1,47 @@
 // you can pass in `scratchData` to test out `findByid`
 // your function
 const scratchData = [
-    { id: 22, foo: 'bar' },
-    { id: 28, foo: 'bizz' },
-    { id: 19, foo: 'bazz' },
+  { id: 22, foo: "bar" },
+  { id: 28, foo: "bizz" },
+  { id: 19, foo: "bazz" }
+];
+
+function findById(items, idNum) {
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].id == idNum) {
+      return items[i];
+    }
+  }
+
+  return null;
+}
+
+//
+
+function testIt() {
+  const testData = [
+    { id: 1, foo: "bar" },
+    { id: 2, foo: "bizz" },
+    { id: 3, bang: "boo" }
   ];
-  
-  function findById(items, idNum) {
-      let foundItem = null;
-
-      items.forEach(function(item) {
-        if (item.id == idNum) {
-            foundItem = item;
-        };
-      });
-
-      return foundItem;
+  const result = findById(testData, 3);
+  if (!(result && result !== null && typeof result === "object")) {
+    console.error("`findById` must return an object");
+    return;
   }
-  
-  //
-  
-  function testIt() {
-    const testData = [
-      { id: 1, foo: 'bar' },
-      { id: 2, foo: 'bizz' },
-      { id: 3, bang: 'boo' },
-    ];
-    const result = findById(testData, 3);
-    if (!(result && result !== null && typeof result === 'object')) {
-      console.error('`findById` must return an object');
-      return;
-    }
-    if (result.id !== 3) {
-      console.error(
-        'Asked for item with id of `3` but got back one with id of ' + result.id
-      );
-      return;
-    }
-    if (result.bang !== 'boo') {
-      console.error(
-        'Expected all key/value pairs from target object to be returned'
-      );
-      return;
-    }
-    console.log('SUCCESS: `findByid` is working');
+  if (result.id !== 3) {
+    console.error(
+      "Asked for item with id of `3` but got back one with id of " + result.id
+    );
+    return;
   }
-  
-  testIt();
-  
+  if (result.bang !== "boo") {
+    console.error(
+      "Expected all key/value pairs from target object to be returned"
+    );
+    return;
+  }
+  console.log("SUCCESS: `findByid` is working");
+}
+
+testIt();
